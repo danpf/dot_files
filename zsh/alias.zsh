b8d8b93885179f3daa341be7486c036129a7b533
Danny Farrell <danpf@uw.edu>
2017-06-20 10:58:31 -0700

    Changes for the new digs

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,6 +8,6 @@ alias tmux='tmux -2'
 alias mhyak='sshfs danpf@hyak:/suppscr/dimaio/danpf/ ~/mount/hyak/'
 
 # sshfs digs
-alias sd='ssh -f danpf@fw.bakerlab.org -L 2222:dig1:22 -N'
-alias md='sshfs -p 2222 danpf@localhost:/work/danpf ~/mount/digs'
+alias sd='ssh -f danpf@fw.bakerlab.org -L 2222:digs:22 -N'
+alias md='sshfs -p 2222 danpf@localhost:/home/danpf ~/mount/digs'
 
