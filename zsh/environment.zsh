1a79b3c13308faa3728c356bd8c6ce0485d47041
Dan Farrell <danpf@uw.edu>
2017-10-12 14:01:28 -0700

    Fixes

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -4,10 +4,6 @@
 export TERM=screen-256color
 [[ $TMUX = "" ]] && export TERM="xterm-256color"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Vim mode
 set -o vi
 
