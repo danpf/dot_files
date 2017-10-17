81e10d8bb9e76299607ac0f57fb3f31e7dcb75fb
Danny Farrell <danpf@uw.edu>
2017-10-17 15:25:50 -0700

    Fixed some hyak stuff

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,6 +12,11 @@ alias sshdigs='ssh danpf@fw.bakerlab.org'
 alias sd='ssh -f danpf@fw.bakerlab.org -L 2222:digs:22 -N'
 alias md='sshfs -p 2222 danpf@localhost:/home/danpf ~/mount/digs'
 
+# submit jobs to hyak
+bf () {for job in $(seq 1 $1); do qsub -q bfwait submit.pbs; done;}
+dm () {for job in $(seq 1 $1); do qsub -W group_list=hyak-dimaio submit.pbs; done;}
+
+
 # Function needs to be before alias
 pathmunge () {
         if ! echo "$PATH" | /bin/grep -Eq "(^|:)$1($|:)" ; then
