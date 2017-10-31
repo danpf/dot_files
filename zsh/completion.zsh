be4c8e570cd459006bcdf48645ac8595655b7766
Dan Farrell <danpf@uw.edu>
2017-10-31 15:06:31 -0700

    Fixed no colors in tab completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -21,7 +21,7 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 
 
 
-#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 #zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
 #zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
 #zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
